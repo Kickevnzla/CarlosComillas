2e7ae675b1140980db154226524d105ae9ebc7a7
Jesus Paredes <kickevnzla@gmail.com>
2024-03-26 20:48:48 -0300

    Carousell fixed

--- src/components/ClientsGallery/ClientsGallery.jsx
+++ src/components/ClientsGallery/ClientsGallery.jsx
@@ -1,9 +1,10 @@
 import styles from './styles/ClientsGalleryStyled.module.scss';
 import Container from '../Container';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { IoClose } from 'react-icons/io5';
 import { motion, AnimatePresence } from 'framer-motion';
 import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
+import clients from '../../assets/clients.json';
 import {
 	CarouselProvider,
 	Slider,
@@ -12,52 +13,6 @@ import {
 	ButtonNext
 } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
-
-import {
-	LogoAfiko,
-	LogoAngelMusicEvents,
-	LogoBSG,
-	LogoBucare,
-	LogoDTJ,
-	LogoCaminoAlSer,
-	LogoByGaby,
-	LogoHey,
-	LogoHolyDonas,
-	LogoLucchy,
-	LogoTasty,
-	LogoUCPL,
-	AplicacionAfiko,
-	AplicacionAngelMusicEvents,
-	AplicacionBSG,
-	AplicacionBucare,
-	AplicacionByGaby,
-	AplicacionCaminoAlSer,
-	AplicacionDTJ,
-	AplicacionHey,
-	AplicacionHolyDonas,
-	AplicacionLucchy,
-	AplicacionTasty,
-	AplicacionUCPL
-} from '../../images';
-
-const logos = [
-	{ id: 1, logo: LogoAfiko, aplication: AplicacionAfiko },
-	{
-		id: 2,
-		logo: LogoAngelMusicEvents,
-		aplication: AplicacionAngelMusicEvents
-	},
-	{ id: 3, logo: LogoBSG, aplication: AplicacionBSG },
-	{ id: 4, logo: LogoBucare, aplication: AplicacionBucare },
-	{ id: 5, logo: LogoDTJ, aplication: AplicacionDTJ },
-	{ id: 6, logo: LogoCaminoAlSer, aplication: AplicacionCaminoAlSer },
-	{ id: 7, logo: LogoByGaby, aplication: AplicacionByGaby },
-	{ id: 8, logo: LogoHey, aplication: AplicacionHey },
-	{ id: 9, logo: LogoHolyDonas, aplication: AplicacionHolyDonas },
-	{ id: 10, logo: LogoLucchy, aplication: AplicacionLucchy },
-	{ id: 11, logo: LogoTasty, aplication: AplicacionTasty },
-	{ id: 12, logo: LogoUCPL, aplication: AplicacionUCPL }
-];
 
 function ClientsGallery() {
 	const [isVisible, setIsVisible] = useState(false);
@@ -74,7 +29,7 @@ function ClientsGallery() {
 					<CarouselProvider
 						naturalSlideWidth={400}
 						naturalSlideHeight={400}
-						totalSlides={2}
+						totalSlides={3}
 						isIntrinsicHeight={true}
 						infinite={true}
 						visibleSlides={1}
@@ -86,25 +41,28 @@ function ClientsGallery() {
 							<ButtonBack className={styles.arrow}>
 								<FaArrowCircleLeft />
 							</ButtonBack>
+
 							<Slider className={styles.slider}>
-								<Slide className={styles.slide} index={0}>
-									<div className={styles.gridContainer}>
-										<div className={styles.clientsLogos}>
-											{logos.map((item, index) => (
-												<div
-													key={index}
-													className={styles.clientLogo}
-													onClick={() => {
-														setIsVisible(true);
-														setTempImage(item.aplication);
-													}}
-												>
-													<img src={item.logo} />
-												</div>
-											))}
+								{clients.slides.map((slide, index) => (
+									<Slide key={index} className={styles.slide} index={index}>
+										<div className={styles.gridContainer}>
+											<div className={styles.clientsLogos}>
+												{slide.map((client, index) => (
+													<div
+														key={index}
+														className={styles.clientLogo}
+														onClick={() => {
+															setIsVisible(true);
+															setTempImage(client.aplicacion);
+														}}
+													>
+														<img src={client.logo} />
+													</div>
+												))}
+											</div>
 										</div>
-									</div>
-								</Slide>
+									</Slide>
+								))}
 							</Slider>
 
 							<ButtonNext className={styles.arrow}>
